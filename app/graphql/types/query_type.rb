08fd410e79fd5a34cb3926ea154187e7c36f65da
Birzhan Serik <birzhans14@gmail.com>
2022-06-20 13:11:14 +0600

    post & author types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,17 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :posts, [Types::PostType], null: false
+    field :authors, [Types::AuthorType], null: false
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def posts
+      Post.all
     end
+
+    def authors
+      Author.all
+    end 
   end
 end
